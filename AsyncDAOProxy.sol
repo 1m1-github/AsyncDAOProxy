254cc048b9a538fa632c31563b4848c70064f186
1m1 <77983409+1m1-github@users.noreply.github.com>
2024-03-04 07:53:07 -0800

    WORK IN PROGRESS (untested)

--- AsyncDAOProxy.sol
+++ AsyncDAOProxy.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// WORK IN PROGRESS (untested)
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AsyncDAOPRoxy is ERC20 {
